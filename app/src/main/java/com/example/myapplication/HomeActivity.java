d68ef91b583c7f0e7151de20cddfab21aa9f7721
fauzul123 <fauzulxrpl2018rus@gmail.com>
2020-10-07 12:56:00 +0700

    update

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -15,6 +15,7 @@ import android.widget.Button;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -46,6 +47,7 @@ public class HomeActivity extends AppCompatActivity {
     private HTTPHandler mAdapter;
     Button btn;
     boolean sds;
+    @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,114 +57,107 @@ public class HomeActivity extends AppCompatActivity {
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
         setContentView(R.layout.activity_home);
-        btn = findViewById(R.id.btnprofil);
+//        btn = findViewById(R.id.btnprofil);
         dataList = findViewById(R.id.dataList);
 //        swp = findViewById(R.id.swipe_refresh2);
-        swp.setOnRefreshListener((SwipeRefreshLayout.OnRefreshListener) this);
-        swp.post(new Runnable(){
-            @Override
-            public void run() {
-                getApi();
-            }
-        });
+//        swp.setOnRefreshListener((SwipeRefreshLayout.OnRefreshListener) this);
+//        swp.post(new Runnable(){
+//            @Override
+//            public void run() {
+//                getApi();
+//            }
+//        });
         dataList.setHasFixedSize(true);
         dataList.setLayoutManager(new LinearLayoutManager(this));
 
-//
-//        titles = new ArrayList<>();
-//        images = new ArrayList<>();
-//        prices = new ArrayList<>();
-//
-//        titles.add("Pacific satu");
-//        titles.add("Pacific dua");
-//        titles.add("Pacific tiga");
-//        titles.add("Pacific empat");
-//
-//        prices.add("5000");
-//        prices.add("8000");
-//        prices.add("7000");
-//        prices.add("9000");
-//
-//        images.add(R.drawable.pacificoriblue);
-//        images.add(R.drawable.bromptonrodaor);
-//        images.add(R.drawable.pacificorblack);
-//        images.add(R.drawable.pacificputihbiru);
-//
-//        adapter = new Adapter(this,titles,images,prices);
-//        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
-//        dataList.setLayoutManager(gridLayoutManager);
-//        dataList.setAdapter(adapter);
-//
-//        Bundle extras = getIntent().getExtras();
-//        String value = extras.getString("userid");
-//        if(extras != null){
-//            String value = extras.getString("userid");
-//        }
+
+        titles = new ArrayList<>();
+        images = new ArrayList<>();
+        prices = new ArrayList<>();
+        titles.add("Sepeda satu");
+        titles.add("Sepeda dua");
+        titles.add("Sepeda tiga");
+        titles.add("Sepeda empat");
+
+        prices.add("5000");
+        prices.add("8000");
+        prices.add("7000");
+        prices.add("9000");
+
+        images.add(R.drawable.pacificoriblue);
+        images.add(R.drawable.logo);
+        images.add(R.drawable.pacificbluedoubt);
+        images.add(R.drawable.pacificcombungu);
+
+        adapter = new Adapter(this,titles,images,prices);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
+        dataList.setLayoutManager(gridLayoutManager);
+        dataList.setAdapter(adapter);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            String value = extras.getString("userid");
+        }
 
     }
 
+
+
+//    public void getApi(){
+////        swp.setRefreshing(true);
+//        AndroidNetworking.get(Config.BASE_URL+"getitem")
+//                .setPriority(Priority.MEDIUM)
+//                .setOkHttpClient(((RS) getApplication()).getOkHttpClient())
+//                .build()
+//                .getAsJSONObject(new JSONObjectRequestListener() {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+////                        swp.setRefreshing(false);
+//                        if (mAdapter != null) {
+//                            mAdapter.clearData();
+//                            mAdapter.notifyDataSetChanged();
+//                        }
+//                        if (mList != null) mList.clear();
+//                        Log.d("RBA", "res" + response);
 //
-//    public void btnprofil(View view){
-//        Intent intent = new Intent(HomeActivity.this,DetailActivity.class);
-//        startActivity(intent);
+//                        String status = response.optString(Config.RESPONSE_STATUS_FIELD);
+//                        String message = response.optString(Config.RESPONSE_MESSAGE_FIELD);
+//                        if (message.trim().equalsIgnoreCase(Config.RESPONSE_STATUS_VALUE_SUCCESS)) {
+//                            JSONArray payload = response.optJSONArray(Config.RESPONSE_PAYLOAD_FIELD);
+//
+//                            if (payload == null) {
+//                                Toast.makeText(HomeActivity.this, "Tidak ada user", Toast.LENGTH_SHORT).show();
+//                                return;
+//                            }
+//                            for (int i = 0; i < payload.length(); i++) {
+//                                JSONObject dataUser = payload.optJSONObject(i);
+//                                ModelUserAdmin item = new ModelUserAdmin(dataUser);
+//                                item.setIditem(dataUser.optInt("id"));
+//                                item.setMerk(dataUser.optString("merk"));
+//                                item.setKodesepeda(dataUser.optString("kodsepeda"));
+//                                item.setWarna(dataUser.optString("warna"));
+//                                item.setGambar(dataUser.optString("gambar"));
+//                                item.setUrl(dataUser.optString("url_image"));
+//                                item.setHarga(dataUser.optString("harga"));
+//                                mList.add(item);
+//                            }
+//                            mAdapter = new HTTPHandler(HomeActivity.this, mList, HomeActivity.this);
+//                            dataList.setAdapter(mAdapter);
+//                        } else {
+//                            Toast.makeText(HomeActivity.this, message, Toast.LENGTH_SHORT).show();
+//                            JSONObject payload = response.optJSONObject(Config.RESPONSE_PAYLOAD_FIELD);
+//                            if (payload != null && payload.optString("API_ACTION").equalsIgnoreCase("LOGOUT"))
+//                                Config.forceLogout(HomeActivity.this);
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onError(ANError anError) {
+//
+//                    }
+//                });
+//
 //    }
-
-
-    public void getApi(){
-//        swp.setRefreshing(true);
-        AndroidNetworking.get(Config.BASE_URL+"getitem")
-                .setPriority(Priority.MEDIUM)
-                .setOkHttpClient(((RS) getApplication()).getOkHttpClient())
-                .build()
-                .getAsJSONObject(new JSONObjectRequestListener() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-//                        swp.setRefreshing(false);
-                        if (mAdapter != null) {
-                            mAdapter.clearData();
-                            mAdapter.notifyDataSetChanged();
-                        }
-                        if (mList != null) mList.clear();
-                        Log.d("RBA", "res" + response);
-
-                        String status = response.optString(Config.RESPONSE_STATUS_FIELD);
-                        String message = response.optString(Config.RESPONSE_MESSAGE_FIELD);
-                        if (message.trim().equalsIgnoreCase(Config.RESPONSE_STATUS_VALUE_SUCCESS)) {
-                            JSONArray payload = response.optJSONArray(Config.RESPONSE_PAYLOAD_FIELD);
-
-                            if (payload == null) {
-                                Toast.makeText(HomeActivity.this, "Tidak ada user", Toast.LENGTH_SHORT).show();
-                                return;
-                            }
-                            for (int i = 0; i < payload.length(); i++) {
-                                JSONObject dataUser = payload.optJSONObject(i);
-                                ModelUserAdmin item = new ModelUserAdmin(dataUser);
-                                item.setIditem(dataUser.optInt("id"));
-                                item.setMerk(dataUser.optString("merk"));
-                                item.setKodesepeda(dataUser.optString("kodsepeda"));
-                                item.setWarna(dataUser.optString("warna"));
-                                item.setGambar(dataUser.optString("gambar"));
-                                item.setUrl(dataUser.optString("url_image"));
-                                item.setHarga(dataUser.optString("harga"));
-                                mList.add(item);
-                            }
-                            mAdapter = new HTTPHandler(HomeActivity.this, mList, HomeActivity.this);
-                            dataList.setAdapter(mAdapter);
-                        } else {
-                            Toast.makeText(HomeActivity.this, message, Toast.LENGTH_SHORT).show();
-                            JSONObject payload = response.optJSONObject(Config.RESPONSE_PAYLOAD_FIELD);
-                            if (payload != null && payload.optString("API_ACTION").equalsIgnoreCase("LOGOUT"))
-                                Config.forceLogout(HomeActivity.this);
-                        }
-                    }
-
-                    @Override
-                    public void onError(ANError anError) {
-
-                    }
-                });
-
-    }
 
     @Override
     public void onBackPressed() {
